2c19bcb6dd8c6ae192c69abf650b26b85ba00fb9
Adam Sinnett <adam.sinnett@jivesoftware.com>
2013-07-13 12:42:21 -0700

    [Router] [SplashScreen] [chatListView]
    Finalized application router
    SplashScreen with install button and login
    ChatListView

---
+++ js/views/splash_screen_view.js
@@ -0,0 +1,38 @@
+(function() {
+'use static';
+  var FirefoxIM = window.FirefoxIM || {};
+  FirefoxIM.Views = FirefoxIM.Views || {};
+
+  FirefoxIM.Views.SplashScreenView = Backbone.View.extend({
+
+    el: FirefoxIM.Templates.splashScreenView(),
+
+    events: {
+      "click #signin": "siginIn",
+    },
+
+    initialize: function(firebaseRef, options) {
+      this.auth = new FirebaseSimpleLogin(firebaseRef, this.authCallback);
+      this.install = new FirefoxIM.Views.InstallView();
+    },
+
+    render: function() {
+      $(document.body).append(this.$el);
+      return this;
+    },
+
+    authCallback: function(error, user) {
+      if (error) {
+        FirefoxIM.router.navigate('error', {trigger: true});
+      } else if (user) {
+        FirefoxIM.router.navigate('chatList', {trigger: true});
+      }
+    },
+
+    siginIn: function() {
+      this.auth.login('persona');
+    }
+  });
+
+  window.FirefoxIM = FirefoxIM;
+}())
