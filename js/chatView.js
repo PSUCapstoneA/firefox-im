c50e9ded831996a44a872464513afb33c54decda
thechuck <cherb@pdx.edu>
2013-07-15 15:40:34 -0700

    # This is a combination of 2 commits. # The first commit's message is:
    deleted test files
    
    Able to send name and message to firebase
    
    Fixed Variables outside of namespace
    
    Sanitized input for name and message text fields.  Can no logner run scripts from the input line.
    
    Broke the objects up into different .js files
    
    moved the testfiles to new folder
    
    moved the testfiles to new folder
    
    Testing Folder Commit
    
    # This is the 2nd commit message:
    
    Unstaged changes

--- js/chatView.js
+++ js/chatView.js
@@ -14,8 +14,8 @@ FirefoxIM.ChatView = Backbone.View.extend({
 			if(messages != null){
 				for (var property in messages){
 						var idAttr = "\"" + property + "\""; 
-						var name = messages[property].userID;			
-						var text = FirefoxIM.encodeHTML(messages[property].chatText);	
+						var name = messages[property].userid;			
+						var text = messages[property].message;	
 						if(!viewObject.hasMessageAlready(property))
 							viewObject.render(name,text,idAttr);
 				}	
@@ -52,13 +52,14 @@ FirefoxIM.encodeHTML = function(s) {
    return s.replace(/&[^(amp;)(lt;)(quot;)(gt;)(nbsp;)]/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/>/g, '&gt;').replace(/ /, '&nbsp;');
 
 }
+var chatview = new FirefoxIM.ChatView({firebase: new Firebase("http://psucapstone-a.firebaseio.com/chats/messages")});
 
 //setChat function takes the value for name and text puts them into the chat model and
 //then calls the putChat function to place the chat into the chats collection
 FirefoxIM.setChat = function (){
-	var userID = FirefoxIM.encodeHTML($('#user').val());
+	var userID = "";
 	var chatText = FirefoxIM.encodeHTML($('#chatText').val());
-	FirefoxIM.chat.set({userID: userID, chatText: chatText});
+	FirefoxIM.chat.set({userid: userID, message: chatText});
 	FirefoxIM.putChat(FirefoxIM.chat);	
 	
 }
