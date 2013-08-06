7e6d9af9020ca3bd81aa9bfee2592bf2c19fc9b3
liubrian <xinlongbrian@gmail.com>
2013-08-05 22:43:22 -0700

    made change in newchat

--- js/router.js
+++ js/router.js
@@ -33,7 +33,7 @@
     },
 
     newChat: function() {
-      this.getChatList().add(new FirefoxIM.Models.Chat());
+      this.getChatList().add(new FirefoxIM.Models.Chat({user:{jeff:true,brian:true}}));
       this.renderParentView(FirefoxIM.Views.ChatView, _.last(this.getChatList().models));
     },
 
