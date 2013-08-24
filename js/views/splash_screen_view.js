672dc17e173e3c0663f109773a2e7ca1b3edb00d
Adam Sinnett <adam.sinnett@jivesoftware.com>
2013-08-24 12:18:01 -0700

    Settings Works again, bug fixes, updates to Firefox Styles

--- js/views/splash_screen_view.js
+++ js/views/splash_screen_view.js
@@ -12,8 +12,8 @@
     },
 
     initialize: function(firebaseRef, options) {
-      FirefoxIM.auth = new FirebaseSimpleLogin(firebaseRef, this.authCallback);
-      FirefoxIM.auth.logout();
+      this.options = options;
+      this.auth = new FirebaseSimpleLogin(firebaseRef, _.bind(this.authCallback, this));
       this.install = new FirefoxIM.Views.InstallView();
     },
 
@@ -26,19 +26,24 @@
       if (error) {
         FirefoxIM.router.navigate('error', {trigger: true});
       } else if (user) {
-        FirefoxIM.user = user;
-        
-        if(!FirefoxIM.userList.findWhere({id: FirefoxIM.user.id})){
-          FirefoxIM.router.navigate("newUser", {trigger: true});
-          return;
-        }  
-        
-        FirefoxIM.router.navigate("chatList", {trigger: true});
+        if (this.options.logout) {
+          this.auth.logout();
+          this.options.logout = false;
+        } else {
+          FirefoxIM.user = user;
+          
+          if(!FirefoxIM.userList.findWhere({id: FirefoxIM.user.id}) && FirefoxIM.userList.length !== 0){
+            FirefoxIM.router.navigate("newUser", {trigger: true});
+            return;
+          }  
+          
+          FirefoxIM.router.navigate("chatList", {trigger: true});
+        }
       }
     },
 
     siginIn: function() {
-      FirefoxIM.auth.login('persona');
+      this.auth.login('persona');
     }
   });
 
