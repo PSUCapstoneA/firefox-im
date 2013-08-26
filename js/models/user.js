6e41662315692deeea9689ce7bf0c7821449049a
Fargo <jeffwest_98@Jeffreys-MacBook-Pro.local>
2013-08-26 12:31:31 -0700

    Fixed bug where contact list for user would be deleted when editing account info. Adding orange banner on top of user view to make app look consistent.

--- js/models/user.js
+++ js/models/user.js
@@ -5,6 +5,9 @@
   FirefoxIM.Models = FirefoxIM.Models || {};
 
   FirefoxIM.Models.User =  Backbone.Model.extend({
+	encodeHTML: function(s) {
+      return s.replace(/&[^(amp;)(lt;)(quot;)(gt;)(nbsp;)]/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/>/g, '&gt;').replace(/ /g, '&nbsp;').replace(/\n/g,'<br>');
+    }   
   });
   window.FirefoxIM = FirefoxIM;
 }())
